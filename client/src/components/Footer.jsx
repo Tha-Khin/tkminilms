66dc584034491b92cbf2b35d73b8ad62ec941681
Tha_Khin <thakhin.pp@gmail.com>
2025-11-25 01:44:14 +0700

    add currentYear

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,12 @@
-
+const currentYear = new Date().getFullYear();
 const Footer = () => {
   return (
-    <footer className='bg-gray-900 md:px-36 text-left w-full'>
-      <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 @ TK Mini LMS. All Right Reserved.</p>
+    <footer className="bg-gray-900 md:px-36 text-left w-full">
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">
+        Copyright {currentYear} @ TK Mini LMS. All Right Reserved.
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
