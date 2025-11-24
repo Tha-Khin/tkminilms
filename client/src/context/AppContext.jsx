d1f572a913b5721ed6f12d3c2a0a5bc3a942b90f
Tha_Khin <thakhin.pp@gmail.com>
2025-11-25 00:30:26 +0700

    change time words

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,65 +1,86 @@
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { toast } from "react-toastify";
-import humanizeDuration from 'humanize-duration'
+import humanizeDuration from "humanize-duration";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-export const AppContextProvider = (props)=>{
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const token = localStorage.getItem("token");
-    const expiry = localStorage.getItem("expiry");
-    const [allCourses, setAllCourses] = useState([])
+export const AppContextProvider = (props) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const token = localStorage.getItem("token");
+  const expiry = localStorage.getItem("expiry");
+  const [allCourses, setAllCourses] = useState([]);
 
-    // Fetch All Courses
-    const fetchAllCourses = async ()=>{
-        try {
-            const {data} = await axios.get(backendUrl + '/api/courses/all')
-            if(data.success){
-                setAllCourses(data.data)
-            }else{
-                toast.error("Error in Fetching Courses")
-            }
-        } catch (error) {
-            toast.error(error.message)
-        }
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/courses/all");
+      if (data.success) {
+        setAllCourses(data.data);
+      } else {
+        toast.error("Error in Fetching Courses");
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    const calculateCourseDuration = (course)=>{
-      let time = 0
-      JSON.parse(course.lessons).map((lesson)=> time += lesson[2])
-      return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+  const calculateCourseDuration = (course) => {
+    let time = 0;
+    JSON.parse(course.lessons).map((lesson) => (time += lesson[2]));
+    return humanizeDuration(time * 60 * 1000, {
+      units: ["h", "m"],
+      round: true,
+      language: "short_en",
+      languages: {
+        short_en: {
+          h: () => "hr",
+          m: () => "min",
+        },
+      },
+      delimiter: ", ",
+    });
+  };
+
+  const logout = async () => {
+    setLoading(true);
+    const { data } = await axios.post(
+      backendUrl + "/api/auth/logout",
+      { action: "logout", token },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    if (data.success) {
+      toast.success(data.message);
+      localStorage.clear();
+      navigate("/login");
+      setLoading(false);
+    } else {
+      toast.error(data.message);
+      localStorage.clear();
+      setLoading(false);
     }
+  };
 
-    const logout = async () => {
-        setLoading(true);
-        const {data} = await axios.post(backendUrl + '/api/auth/logout', { action: "logout", token }, {headers: {'Content-Type': 'application/json'}});
-        if(data.success){
-            toast.success(data.message);
-            localStorage.clear();
-            navigate("/login");
-            setLoading(false);
-        }else{
-            toast.error(data.message);
-            localStorage.clear();
-            setLoading(false);
-        }
-    };
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
 
-    useEffect(()=>{
-        fetchAllCourses()
-    }, [])
+  const value = {
+    allCourses,
+    navigate,
+    token,
+    expiry,
+    logout,
+    backendUrl,
+    calculateCourseDuration,
+    loading,
+    setLoading,
+  };
 
-    const value = {
-        allCourses, navigate, token, expiry, logout, backendUrl, calculateCourseDuration, loading, setLoading
-    }
-
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
